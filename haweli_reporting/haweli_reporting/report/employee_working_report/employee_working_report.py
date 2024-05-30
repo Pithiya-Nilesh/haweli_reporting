e04d099f70515c07282547acaa1475b10d21e573
Nilesh-Pithiya <nilesh@webisoft.com>
2024-05-30 17:00:48 +0530

    changes in report

--- haweli_reporting/haweli_reporting/report/employee_working_report/employee_working_report.py
+++ haweli_reporting/haweli_reporting/report/employee_working_report/employee_working_report.py
@@ -3,8 +3,8 @@ import frappe
 def execute(filters=None):
     columns, data = [], []
 
-    
     columns = [
+       
         {
             "label": "Employee Name",
             "fieldname": "employee_name",
@@ -17,24 +17,38 @@ def execute(filters=None):
             "fieldtype": "Date",
             "width": 150,
         },
-      
+        {
+            "label": "Start Time",
+            "fieldname": "start_time",
+            "fieldtype": "Time",
+            "width": 150,
+        },
+        {
+            "label": "End Time",
+            "fieldname": "end_time",
+            "fieldtype": "Time",
+            "width": 150,
+        },
         {
             "label": "Working Hours",
             "fieldname": "working_hours",
             "fieldtype": "Float",
             "width": 150,
         },
-        
+        {
+            "label": "Description",
+            "fieldname": "description",
+            "fieldtype": "Data",
+            "width": 350,
+        },
     ]
     
-   
     logged_in_user = frappe.session.user
     
     is_admin_or_specific_user = (
         "Administrator" in frappe.get_roles(logged_in_user) or 
         logged_in_user == "anand@gmail.com"
     )
-    
     
     if not is_admin_or_specific_user:
         permitted_users = [d.for_value for d in frappe.get_all("User Permission", filters={"user": logged_in_user}, fields=["for_value"])]
@@ -44,7 +58,6 @@ def execute(filters=None):
     else:
         permitted_users = []
 
-   
     sql = """
         SELECT 
             tr.first_name AS employee_name,
@@ -52,12 +65,15 @@ def execute(filters=None):
             ti.date AS date,
             ti.time AS start_time,
             ti.end_time AS end_time,
-            SUM(ti.working_hours) AS working_hours,
+            ti.working_hours AS working_hours,
             ti.description AS description
         FROM 
             `tabTask Reporting` tr
+        
         JOIN 
             `tabTiming Report` ti ON ti.parent = tr.name
+            
+        
     """
     
     conditions = []
@@ -67,7 +83,6 @@ def execute(filters=None):
         sql += " WHERE tr.user IN ({})".format(", ".join(["%s"] * len(permitted_users)))
         sql_args.extend(permitted_users)
 
-   
     if filters.get("start_date"):
         conditions.append("ti.date >= %s")
         sql_args.append(filters.get("start_date"))
@@ -83,9 +98,28 @@ def execute(filters=None):
             sql += " AND " + " AND ".join(conditions)
         else:
             sql += " WHERE " + " AND ".join(conditions)
-        
-    sql += " GROUP BY tr.first_name, ti.date"
+     
+    sql += " ORDER BY tr.user ASC, ti.date ASC"        
 
     data = frappe.db.sql(sql, tuple(sql_args), as_dict=True)
+
+    
+    if data:
+        new_data = []
+        prev_row = data[0]
+        for row in data:
+            if row["user"] != prev_row["user"] or row["date"] != prev_row["date"]:
+                 new_data.append({
+                    "employee_name": None,
+                    "date": None,
+                    "start_time": None,
+                    "end_time": None,
+                    "working_hours": None,
+                    "description": None,
+
+                })
+            new_data.append(row)
+            prev_row = row
+        data = new_data
 
     return columns, data
